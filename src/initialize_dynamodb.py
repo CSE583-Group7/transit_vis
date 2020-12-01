1652b2e0af10ab02fcd7de97bfd2ae8480251789
zackAemmer <zae5op@gmail.com>
2020-12-01 00:37:18 -0800

    Rewrote scripts as functions

---
+++ src/initialize_dynamodb.py
@@ -0,0 +1,196 @@
+import json
+import os
+
+import boto3
+import numpy as np
+
+import config as cfg
+
+
+def sort_and_save_exploded_lines(exploded_geojson_name, output_folder):
+    with open(f"{exploded_geojson_name}.geojson", 'r') as f:
+        kcm_routes = json.load(f)
+
+    # Sort route data by its route label to make processing it in the next step faster
+    data = kcm_routes['features']
+    label_sorted = {}
+    for datapoint in data:
+        k = datapoint['properties']['ROUTE_ID']
+        if k not in label_sorted.keys():
+            label_sorted[k] = [datapoint]
+        else:
+            label_sorted[k].append(datapoint)
+
+
+    for key in label_sorted.keys():
+        # Open file specific to each label
+        with open(f"{output_folder}/label_{key}.json", "w+") as outfile:
+            json.dump(label_sorted[key], outfile)
+
+    # Save a list of keys
+    with open(f"{output_folder}/labels.json", "w+") as outfile:
+        json.dump(list(label_sorted.keys()), outfile)
+    return 1
+
+def join_line_segments(input_folder, min_segment_length):    
+    # Matching route segments to get segments that are connected and larger than min_segment_length meters
+    feature_list = []
+    isolated_list = []
+    keys = []
+    with open(f"{input_folder}/labels.json", "r") as keyfile:
+        keys = json.load(keyfile)
+    for k in keys:
+        with open(f"{input_folder}/label_{k}.json", "r") as f:
+            data = json.load(f)
+            i = 0
+            isolated_segments = []
+            while i < len(data):
+                if data[i]['properties']['SEG_LENGTH'] > min_segment_length:
+                    i += 1
+                    continue
+                segment = data.pop(i)
+                start = segment['geometry']['coordinates'][0]
+                end = segment['geometry']['coordinates'][-1]
+                flag = False
+                for j in range(0, len(data)):
+                    seg2 = data[j]
+                    if end == seg2['geometry']['coordinates'][0]:
+                        seg2['geometry']['coordinates'].append(segment['geometry']['coordinates'][0])
+                        seg2['properties']['SEG_LENGTH'] += segment['properties']['SEG_LENGTH']
+                        flag = True
+                        break
+                    elif start == seg2['geometry']['coordinates'][-1]:
+                        seg2['geometry']['coordinates'].append(segment['geometry']['coordinates'][-1])
+                        seg2['properties']['SEG_LENGTH'] += segment['properties']['SEG_LENGTH']
+                        flag = True
+                        break
+                if not flag:
+                    isolated_segments.append(segment)
+            feature_list.extend(data)
+            isolated_list.extend(isolated_segments)
+    return (feature_list, isolated_list, keys)
+
+# Function that iterates through an obj containing nested dicts and lists to replace all floats with strings
+# This is necessary because Dynamodb does not support float values
+def replace_floats(obj):
+    if isinstance(obj, list):
+        for i in range(0,len(obj)):
+            obj[i] = replace_floats(obj[i])
+        return obj
+    elif isinstance(obj, dict):
+        for k in obj.keys():
+            obj[k] = replace_floats(obj[k])
+        return obj
+    elif isinstance(obj, float):
+        if obj % 1 == 0:
+            return int(obj)
+        else:
+            return str(obj)
+    else:
+        return obj
+
+def json_route_ids_to_keys(kcm_routes):
+    # Add the local express to the route id to create unique key for each segment (dynamodb only can have a 2-composite key)
+    for feature in kcm_routes['features']:
+        feature['route_id'] = feature['properties']['ROUTE_ID']
+        feature['segment_id'] = feature['properties']['SEG_ID']
+        if feature['properties']['LOCAL_EXPR'] == 'L':
+            feature['route_id'] = int(str(feature['route_id']) + str(0))
+        else:
+            feature['route_id'] = int(str(feature['route_id']) + str(1))
+    return(kcm_routes)
+
+def connect_to_dynamo():
+    # Set up the connection to the Dynamodb database
+    try:
+        dynamodb = boto3.resource('dynamodb',
+                                 region_name=cfg.REGION,
+                                 aws_access_key_id=cfg.ACCESS_ID,
+                                 aws_secret_access_key=cfg.ACCESS_KEY)
+    except:
+        pass
+    return dynamodb
+
+def create_dynamo_table(dynamodb_resource, table_name):
+    # Create table with one key on the route_id for each route
+    # The rest of the table structure can be open, only required values on insert are keys
+    table = dynamodb_resource.create_table(TableName=table_name,
+                                          KeySchema=[
+                                              {
+                                                  'AttributeName': 'route_id',
+                                                  'KeyType': 'HASH'
+                                              },
+                                              {
+                                                  'AttributeName': 'segment_id',
+                                                  'KeyType': 'RANGE'
+                                              }
+                                          ],
+                                          AttributeDefinitions=[
+                                              {
+                                                  'AttributeName': 'route_id',
+                                                  'AttributeType': 'N'
+                                              },
+                                              {
+                                                  'AttributeName': 'segment_id',
+                                                  'AttributeType': 'N'
+                                              }
+                                          ],
+                                          ProvisionedThroughput={
+                                              'ReadCapacityUnits': 20,
+                                              'WriteCapacityUnits': 20
+                                          })
+    # Wait until the table exists.
+    table.meta.client.get_waiter('table_exists').wait(TableName=table_name)
+    return table
+
+def upload_segments_to_dynamo(dynamodb_resource, table_name, kcm_routes):
+    table = dynamodb_resource.Table(table_name)
+    # Takes about 1hr to do 70,000 features; capacity constrained to stay in AWS free-tier
+    with table.batch_writer() as batch:
+        for route in kcm_routes['features']:
+            batch.put_item(Item={
+                'route_id': route['route_id'],
+                'segment_id': route['segment_id'],
+                'local_express_code': route['properties']['LOCAL_EXPR'],
+                'historic_speeds': [],
+                'avg_speed_m_s': 0
+            })
+    return 1
+
+def main_function(exploded_geojson_name, label_folder_name, dynamodb_table_name):
+    # Create folder to store labels if it doesn't exist
+    try:
+        os.mkdir(label_folder_name, mode = 0o777)
+    except FileExistsError:
+        pass
+    # Read in exploded bus shapefiles, sort them, and join shorter segments to make a new dataset
+    print("Sorting and saving lines...")
+    sort_and_save_exploded_lines(exploded_geojson_name, label_folder_name)
+    print("Joining lines into feature list...")
+    feature_list, isolated_list, keys = join_line_segments(label_folder_name, 100)
+    print("Saving feature list to KCM geojson...")
+    with open(f"{exploded_geojson_name}.geojson", 'r') as f:
+        kcm_routes = json.load(f)
+    # Replace the features with our newly joined segments
+    kcm_routes['features'] = feature_list
+    # Add code for express or local route
+    kcm_routes = json_route_ids_to_keys(kcm_routes)
+    # Turn all float values (coordinates mostly) into strings in the routes geojson
+    kcm_routes = replace_floats(kcm_routes)
+    # Write the modified segments to geojson, this will be used again in Folium (segments in folium and dynamodb match)
+    with open(f"{exploded_geojson_name}_modified.geojson", 'w+') as f:
+        json.dump(kcm_routes, f)
+
+    # Upload the modified segments to a newly created dynamodb table
+    print("Connecting to Dynamodb...")
+    dynamodb_resource = connect_to_dynamo()
+    print("Creating new table...")
+    create_dynamo_table(dynamodb_resource, dynamodb_table_name)
+    print("Uploading segments to table...")
+    upload_segments_to_dynamo(dynamodb_resource, dynamodb_table_name, kcm_routes)
+
+    # Return the number of features that are in the kcm data
+    return len(kcm_routes['features'])    
+
+num_features = main_function('../data/kcm_routes_exploded', '../data/sorted_labels', 'KCM_Bus_Routes_Modified_test')
+print(f"{num_features} features in data uploaded to dynamodb")
---
+++ src/summarize_rds.py
@@ -0,0 +1,236 @@
+from datetime import datetime
+import decimal
+import json
+import requests
+from zipfile import ZipFile
+
+import boto3
+import numpy as np
+import psycopg2
+import pandas as pd
+from sklearn.neighbors import BallTree
+
+import config as cfg
+
+def convert_cursor_to_tabular(query_result_cursor):
+    # Pull out the variables from the query result cursor
+    all_tracks = []
+    for record in query_result_cursor:
+        track = []
+        for feature in record:
+            track = np.append(track, feature)
+        all_tracks.append(track)
+        
+    # Convert variables to Pandas and return
+    daily_results = pd.DataFrame(all_tracks)
+    colnames = []
+    for col in query_result_cursor.description:
+        colnames.append(col.name)
+    daily_results.columns = colnames
+    daily_results['tripid'] = daily_results['tripid'].astype(int)
+    daily_results['vehicleid'] = daily_results['vehicleid'].astype(int)
+    daily_results['orientation'] = daily_results['orientation'].astype(int)
+    daily_results['scheduledeviation'] = daily_results['scheduledeviation'].astype(int)
+    daily_results['locationtime'] = daily_results['locationtime'].astype(int)
+    daily_results['collectedtime'] = daily_results['collectedtime'].astype(int)
+    return daily_results
+
+def load_modified_geojson(path_to_geojson):
+    with open(path_to_geojson, 'r') as f:
+        kcm_routes = json.load(f)
+    return kcm_routes
+
+def connect_to_rds():
+    try:
+        conn = psycopg2.connect(
+        host=cfg.HOST,
+        database=cfg.DATABASE,
+        user=cfg.UID,
+        password=cfg.PWD)
+    except:
+        pass
+    return conn
+
+def get_last_24hrs_results(conn, rds_limit):
+    # Query the last 24 hours of data from the RDS database
+    end_time = round(datetime.now().timestamp())
+    start_time = end_time - (24*60*60)
+    if rds_limit > 0:
+        query_text = f"SELECT * FROM active_trips_study WHERE collectedtime BETWEEN {start_time} AND {end_time} LIMIT {rds_limit};"
+    else:
+        query_text = f"SELECT * FROM active_trips_study WHERE collectedtime BETWEEN {start_time} AND {end_time};"
+    with conn.cursor() as curs:
+        curs.execute(query_text)
+        daily_results = convert_cursor_to_tabular(curs)
+    return daily_results
+    
+def update_gtfs_route_info():
+    # Get the latest GTFS route - trip data from the KCM FTP server
+    url = 'http://metro.kingcounty.gov/GTFS/google_transit.zip'
+    r = requests.get(url, allow_redirects=True)
+    open('../data/google_transit.zip', 'wb').write(r.content)
+    with ZipFile('../data/google_transit.zip', 'r') as zipObj:
+        zipObj.extractall('../data/google_transit')
+    return 1
+
+def preprocess_trip_data(daily_results):
+    # Remove duplicate trip locations
+    daily_results.drop_duplicates(subset=['tripid','locationtime'], inplace=True)
+    daily_results.sort_values(by=['tripid','locationtime'], inplace=True)
+
+    # Offset tripdistance, locationtime, and tripids by 1
+    daily_results['prev_tripdistance'] = 1
+    daily_results['prev_locationtime'] = 1
+    daily_results['prev_tripid'] = 1
+    daily_results['prev_tripdistance'] = daily_results['tripdistance'].shift(1)
+    daily_results['prev_locationtime'] = daily_results['locationtime'].shift(1)
+    daily_results['prev_tripid'] = daily_results['tripid'].shift(1)
+
+    # Remove NA rows, and rows where tripid is different (last recorded location)
+    daily_results.loc[daily_results.tripid == daily_results.prev_tripid, 'tripid'] = None
+    daily_results.dropna(inplace=True)
+    
+    # Calculate average speed between each location bus is tracked at
+    daily_results.loc[:,'dist_diff'] = daily_results['tripdistance'] - daily_results['prev_tripdistance']
+    daily_results.loc[:,'time_diff'] = daily_results['locationtime'] - daily_results['prev_locationtime']
+    daily_results.loc[:,'avg_speed_m_s'] = daily_results['dist_diff'] / daily_results['time_diff']
+
+    # Remove rows where speed is below 0 or above 30 and round to one decimal place
+    daily_results = daily_results[daily_results['avg_speed_m_s'] >= 0]
+    daily_results = daily_results[daily_results['avg_speed_m_s'] <= 30]
+    daily_results.loc[:,'avg_speed_m_s'] = round(daily_results.loc[:,'avg_speed_m_s'])
+    return daily_results
+
+def route_ids_to_keys(daily_results):
+    # Concat 1 or 0 to the route id to make it fit with dynamoDB key schema
+    daily_results.loc[daily_results['trip_short_name']=='LOCAL', 'route_id'] = daily_results['route_id'].apply(lambda x:
+                                                                                                               int(str(x) + '0'))
+    daily_results.loc[daily_results['trip_short_name']=='EXPRESS', 'route_id'] = daily_results['route_id'].apply(lambda x:
+                                                                                                                 int(str(x) + '1'))
+    return daily_results
+
+def get_nearest(src_points, candidates, k_neighbors=1):
+    """Find nearest neighbors for all source points from a set of candidate points"""
+
+    # Create tree from the candidate points
+    tree = BallTree(candidates, leaf_size=15, metric='haversine')
+
+    # Find closest points and distances
+    distances, indices = tree.query(src_points, k=k_neighbors)
+
+    # Transpose to get distances and indices into arrays
+    distances = distances.transpose()
+    indices = indices.transpose()
+
+    # Get closest indices and distances (i.e. array at index 0)
+    # note: for the second closest points, you would take index 1, etc.
+    closest_idx = indices[0]
+    closest_dist = distances[0]
+    return (closest_idx, closest_dist)
+
+def assign_results_to_segments(kcm_routes, daily_results):
+    # Convert segment data from json format to tabular, so that it can be used with nearest neighbors function
+    feature_coords = []
+    route_ids = []
+    seg_ids = []
+    for feature in kcm_routes['features']:
+        for coord_pair in feature['geometry']['coordinates']:
+            feature_coords.append(coord_pair)
+            route_ids.append(feature['route_id'])
+            seg_ids.append(feature['segment_id'])
+    segments = pd.DataFrame()
+    segments['route_id'] = route_ids
+    segments['segment_id'] = seg_ids
+    segments['lat'] = np.array(feature_coords)[:,0]
+    segments['lon'] = np.array(feature_coords)[:,1]
+    
+    # Go route by route, adding all data points collected from the database to their closest segment that shares their route_id
+    to_upload = pd.DataFrame()
+    route_list = pd.unique(daily_results['route_id'])
+    for route in route_list:
+        route_results = daily_results[daily_results['route_id']==route]
+        route_segments = segments[segments['route_id']==route].reset_index()
+
+        # Use closest index returned by get_nearest to join the closest segment info back to each datapoint
+        if len(route_results) > 0 and len(route_segments) > 0:
+            result_idxs, result_dists = get_nearest(route_results[['lat', 'lon']], route_segments[['lat', 'lon']])
+            route_results = route_results.reset_index().join(route_segments.loc[result_idxs,:].reset_index(), rsuffix='_seg')
+            to_upload = to_upload.append(route_results)
+        else:
+            # A route was tracked by OneBusAway that does not have an id in the King County Metro shapefile
+            print(f"Route {route} was either not tracked, or does not have an id in the KCM shapefile")
+            result_idxs = -1
+            result_dists = -1
+    return to_upload
+
+def connect_to_dynamo_table(table_name):
+    # Set up the connection to the Dynamodb database
+    try:
+        dynamodb = boto3.resource('dynamodb',
+                                 region_name=cfg.REGION,
+                                 aws_access_key_id = cfg.ACCESS_ID,
+                                 aws_secret_access_key = cfg.ACCESS_KEY)
+        table = dynamodb.Table(table_name)
+    except:
+        pass
+    return table
+
+def upload_to_dynamo(dynamodb_table, to_upload):
+    # Summarize the bus delay data (for now, just get the average speed for the day per segment)
+    to_upload = to_upload[['route_id', 'segment_id', 'avg_speed_m_s']]
+    to_upload = to_upload.groupby(['route_id', 'segment_id']).mean().reset_index()
+    to_upload['avg_speed_m_s'] = round(to_upload['avg_speed_m_s'], 1)
+    to_upload['avg_speed_m_s'] = to_upload['avg_speed_m_s'].apply(str)
+    to_upload = to_upload.to_dict(orient='records')
+
+    # Update each route/segment combination with its avg speed for the day
+    for track in to_upload:
+        response = dynamodb_table.update_item(
+            Key={
+                'route_id': track['route_id'],
+                'segment_id': track['segment_id']
+            },
+            UpdateExpression="SET avg_speed_m_s=:speed, historic_speeds=list_append(if_not_exists(historic_speeds, :empty_list), :vals)",
+            ExpressionAttributeValues={
+                ':speed': track['avg_speed_m_s'],
+                ':vals': [track['avg_speed_m_s']],
+                ':empty_list': []
+            })
+    return len(to_upload)
+    
+def main_function(dynamodb_table_name, rds_limit):
+    # Update the current gtfs trip-route info from the King County Metro FTP server
+    print("Updating the GTFS files...")
+    update_gtfs_route_info()
+
+    # Load last 24hrs of scraped data
+    print("Connecting to RDS...")
+    conn = connect_to_rds()
+    print("Querying the last 24 hours of data from RDS (10-20mins)...")
+    daily_results = get_last_24hrs_results(conn, rds_limit)
+    print("Finished query; processing RDS data...")
+    daily_results = preprocess_trip_data(daily_results)
+    
+    # Load the gtfs trip-route info and segment shapefile
+    print("Loading shapefile and GTFS files...")
+    gtfs_trips = pd.read_csv('../data/google_transit/trips.txt')
+    gtfs_trips = gtfs_trips[['route_id', 'trip_id', 'trip_short_name']]
+    kcm_routes = load_modified_geojson('../data/kcm_routes_exploded_modified.geojson')
+
+    # Merge scraped data with the gtfs data to assign route_ids to the scraped data, then update route_id to fit dynamodb key schema
+    print("Merging RDS data with GTFS files...")
+    daily_results = daily_results.merge(gtfs_trips, left_on='tripid', right_on='trip_id')
+    daily_results = route_ids_to_keys(daily_results)
+    
+    # Match the scraped data to its closest segments in the route shapefile
+    print("Matching RDS data to nearest segments in shapefile...")
+    daily_results = assign_results_to_segments(kcm_routes, daily_results)
+
+    # Summarize the last 24 hours of bus movement data, and upload to dynamoDB
+    print("Uploading aggregated segment data to dynamoDB...")
+    table = connect_to_dynamo_table(dynamodb_table_name)
+    success = upload_to_dynamo(table, daily_results)
+    return success
+
+num_segments_updated = main_function('KCM_Bus_Routes_Modified', 1000)
+print(f"Number of segments updated: {num_segments_updated}")
