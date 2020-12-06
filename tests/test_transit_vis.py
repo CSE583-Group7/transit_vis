edcf181f066b0d036257f08395c8d6b10785bede
Kelly Balmes <kbalmes43@gmail.com>
2020-12-06 12:54:58 -0800

    removed dynamodb call from unittest

--- tests/test_transit_vis.py
+++ tests/test_transit_vis.py
@@ -8,6 +8,8 @@ test_smoke(cls) -- smoke test
 
 import unittest
 
+import branca.colormap as cm
+
 from src import transit_vis
 
 
@@ -16,17 +18,34 @@ class TestTransitVis(unittest.TestCase):
     Unittest for the module 'transit_vis'
     """
     @classmethod
-    def test_smoke(cls):
+    def test_smoke_census(cls):
+        """
+        Smoke test for the function 'write_census_data_to_geojson'
+        """
+        s0801_path = './data/s0801'
+        s1902_path = './data/s1902'
+        tract_shapes_path = './data/seattle_census_tracts_2010'
+
+        assert transit_vis.write_census_data_to_geojson(s0801_path,\
+                                                        s1902_path,\
+                                                        tract_shapes_path)\
+                                                        is not None
+
+    def test_smoke_folium_map(cls):
         """
         Smoke test for the module 'transit_vis'
         """
+        
+        segment_file = './data/kcm_routes_exploded_modified'
+        census_file = './data/seattle_census_tracts_2010'
+        linear_cm = cm.LinearColormap(['red', 'green'],\
+                                      vmin=0.5,\
+                                      vmax=100.)
 
-        assert transit_vis.main_function('KCM_Bus_Routes_Modified_Production',
-             './data/s0801',
-             './data/s1902',
-             './data/kcm_routes_exploded_modified', 
-             './data/seattle_census_tracts_2010', 
-             'output_map') is not None
+        assert transit_vis.generate_folium_map(segment_file,\
+                                               census_file, linear_cm) \
+                                                        is not None                                                        
+                                                        
 
 ##############################################################################
 
