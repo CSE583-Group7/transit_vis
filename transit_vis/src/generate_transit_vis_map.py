82d4179a230e99d82c04a5624bfba55d787f6369
zackAemmer <zae5op@gmail.com>
2020-12-12 22:41:31 -0800

    pylint edits vis functions

--- transit_vis/src/generate_transit_vis_map.py
+++ transit_vis/src/generate_transit_vis_map.py
@@ -4,5 +4,4 @@ transit_vis.main_function(table_name='KCM_Bus_Routes',
                          s0801_file='../data/s0801',
                          s1902_file='../data/s1902',
                          segment_file='../data/kcm_routes', 
-                         census_file='../data/seattle_census_tracts_2010', 
-                         output_map_file='output_map')
+                         census_file='../data/seattle_census_tracts_2010')
--- transit_vis/src/vis_functions.py
+++ transit_vis/src/vis_functions.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-""" Module of functions that do not require login
+"""Module for functions that do not require AWS login info (config).
 
-This is a module containing all functions for visualization that 
+This is a module containing all functions for visualization that
 do not require AWS login so that they can be tested during continuous
-integration
+integration.
 """
 
-import folium
 import os
+
+import folium
 import pandas as pd
+
 
 def write_census_data_to_csv(s0801_path, s1902_path, tract_shapes_path):
     """Writes the data downloaded directly from ACS to TIGER shapefiles.
@@ -38,28 +40,31 @@ def write_census_data_to_csv(s0801_path, s1902_path, tract_shapes_path):
     s0801_df = pd.read_csv(f"{s0801_path}.csv")
     s1902_df = pd.read_csv(f"{s1902_path}.csv")
     # Filter each table to only variables of interest, make more descriptive
-    commuters_df = s0801_df[['GEO_ID', 'NAME',
-                            'S0801_C01_001E', 'S0801_C01_009E']]
-    commuters_df.columns = ['GEO_ID', 'NAME',
-                           'total_workers', 'workers_using_transit']
-    commuters_df = commuters_df.loc[1:len(commuters_df),:]
+    commuters_df = s0801_df[[
+        'GEO_ID', 'NAME',
+        'S0801_C01_001E', 'S0801_C01_009E']]
+    commuters_df.columns = [
+        'GEO_ID', 'NAME',
+        'total_workers', 'workers_using_transit']
+    commuters_df = commuters_df.loc[1:len(commuters_df), :]
     commuters_df['GEO_ID'] = commuters_df['GEO_ID'].str[-11:]
 
     # Repeat for s1902
-    households_df = s1902_df[['GEO_ID', 'NAME', 'S1902_C01_001E',
-                             'S1902_C03_001E', 'S1902_C02_008E',
-                             'S1902_C02_020E', 'S1902_C02_021E']]
-    households_df.columns = ['GEO_ID', 'NAME', 'total_households',
-                            'mean_income', 'percent_w_assistance',
-                            'percent_white', 'percent_black_or_african_american']
-    households_df = households_df.loc[1:len(households_df),:]
+    households_df = s1902_df[[
+        'GEO_ID', 'NAME', 'S1902_C01_001E',
+        'S1902_C03_001E', 'S1902_C02_008E',
+        'S1902_C02_020E', 'S1902_C02_021E']]
+    households_df.columns = [
+        'GEO_ID', 'NAME', 'total_households',
+        'mean_income', 'percent_w_assistance',
+        'percent_white', 'percent_black_or_african_american']
+    households_df = households_df.loc[1:len(households_df), :]
     households_df['GEO_ID'] = households_df['GEO_ID'].str[-11:]
 
     # Combine datasets on their census tract ID and write to .csv file
     final_df = pd.merge(commuters_df, households_df, on='GEO_ID').drop(columns=['NAME_x', 'NAME_y'])
     final_df.to_csv(f"{tract_shapes_path}_tmp.csv", index=False)
     return 1
-
 
 def generate_folium_map(segment_file, census_file, colormap):
     """Draws together speed/socioeconomic data to create a Folium map.
@@ -83,10 +88,11 @@ def generate_folium_map(segment_file, census_file, colormap):
         dynamodb.
     """
     # Read in route shapefile and give it the style function above
-    kcm_routes = folium.GeoJson(f"{segment_file}_w_speeds_tmp.geojson",
-                               style_function=lambda feature: {
-                                   'color': colormap(feature['properties']['AVG_SPEED_M_S']),
-                                   'weight': 2})
+    kcm_routes = folium.GeoJson(
+        f"{segment_file}_w_speeds_tmp.geojson",
+        style_function=lambda feature: {
+            'color': colormap(feature['properties']['AVG_SPEED_M_S']),
+            'weight': 2})
     # Read in the census data/shapefile and create a choropleth based on income
     seattle_tracts_df = pd.read_csv(f"{census_file}_tmp.csv")
     seattle_tracts_df['GEO_ID'] = seattle_tracts_df['GEO_ID'].astype(str)
@@ -94,25 +100,27 @@ def generate_folium_map(segment_file, census_file, colormap):
                                          seattle_tracts_df['mean_income'],\
                                          errors='coerce')
     seattle_tracts_df = seattle_tracts_df.dropna()
-    seattle_tracts = folium.Choropleth(geo_data=f"{census_file}.geojson",
-                                      name='Socioeconomic Data',
-                                      data=seattle_tracts_df,
-                                      columns=['GEO_ID', 'mean_income'],
-                                      key_on='feature.properties.GEOID10',
-                                      fill_color='PuBu',
-                                      fill_opacity=0.7,
-                                      line_opacity=0.4,
-                                      legend_name='mean income')
+    seattle_tracts = folium.Choropleth(
+        geo_data=f"{census_file}.geojson",
+        name='Socioeconomic Data',
+        data=seattle_tracts_df,
+        columns=['GEO_ID', 'mean_income'],
+        key_on='feature.properties.GEOID10',
+        fill_color='PuBu',
+        fill_opacity=0.7,
+        line_opacity=0.4,
+        legend_name='mean income')
     # Draw map using the speeds and census data
-    m = folium.Map(location=[47.606209, -122.332069],
-                  zoom_start=11,
-                  prefer_canvas=True)
-    seattle_tracts.add_to(m)
-    kcm_routes.add_to(m)
+    f_map = folium.Map(
+        location=[47.606209, -122.332069],
+        zoom_start=11,
+        prefer_canvas=True)
+    seattle_tracts.add_to(f_map)
+    kcm_routes.add_to(f_map)
     colormap.caption = 'Average Speed (m/s)'
-    colormap.add_to(m)
-    folium.LayerControl().add_to(m)
-    return m
+    colormap.add_to(f_map)
+    folium.LayerControl().add_to(f_map)
+    return f_map
 
 def save_and_view_map(f_map, output_file):
     """Writes Folium Map to an output file and prints the path to the terminal.
